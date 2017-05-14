b2135a1b1b78b99c3110e697217c5b918b3df05d
daaguirr <dagum95@gmail.com>
2017-05-14 15:35:44 -0400

    Initial Commit

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^nombre$',views.nombre, name="nombre")
+    url(r'^login$', views.login, name='login'),
+    url(r'^signup$', views.signup, name='signup'),
+    url(r'^vendedor$', views.seller_profile_page, name='seller_profile_page'),
+    url(r'^productos$', views.products_administration, name='products_administration'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import datetime
 
-
 # Create your views here.
 
-def index(request):
-    return render(request, "index.html", {'time': datetime.datetime.now()})
+from django.shortcuts import render
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
 
 
-def nombre(request):
-    print request
-    return render(request, "nombre.html", {'nombre': request['nombre']})
+def index(req):
+    return render(req, 'app/index.html', {})
+
+
+def login(req):
+    return render(req, 'app/login.html')
+
+
+def signup(req):
+    return render(req, 'app/signup.html', {})
+
+
+def seller_profile_page(req):
+    return render(req, 'app/vendedor-profile-page.html', {})
+
+
+def products_administration(req):
+    return render(req, 'app/gestion-productos.html', {})
